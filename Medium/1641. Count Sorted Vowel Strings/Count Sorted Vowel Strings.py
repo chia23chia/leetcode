4e5385e77ac945f3524ea7010d43b19c443e016b
あずさ <chia23chia@gmail.com>
2021-08-09 16:55:17 +0800

    Update Count Sorted Vowel Strings.py

--- Medium/1641. Count Sorted Vowel Strings/Count Sorted Vowel Strings.py
+++ Medium/1641. Count Sorted Vowel Strings/Count Sorted Vowel Strings.py
@@ -1,3 +1,3 @@
 class Solution:
-    def countVowelStrings(self, n: int) -> int: #排組問題 5個箱子放n顆球
+    def countVowelStrings(self, n: int) -> int: #排組問題 5個箱子放n顆球 H 5 n -> C 5+n-1 n
         return math.comb(n + 4, n)
