a6684c28581a56393326d9b710ff6fb14da52cde
あずさ <chia23chia@gmail.com>
2021-09-02 02:26:27 +0800

    Create Binary Search Tree to Greater Sum Tree.cpp

---
+++ Medium/1038. Binary Search Tree to Greater Sum Tree/Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int sum = 0 ;
+public:
+    void fun(TreeNode* r){
+        if(r == NULL) return ;
+        fun(r->right) ;
+        sum += r->val ;
+        r->val = sum ;
+        fun(r->left) ;
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        fun(root) ; 
+        return root ;
+    }
+};
